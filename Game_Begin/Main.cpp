2f729c2fd82eb504faa2c742c586f3b7453e97ef
BernardBussy <213533986@stu.ukzn.ac.za>
2015-04-25 23:32:18 +0200

    Incorporated User Input Via Keyboard and Mouse

--- Game_Begin/Main.cpp
+++ Game_Begin/Main.cpp
@@ -3,6 +3,7 @@
 #include <allegro5\allegro_primitives.h>
 
 enum KBRDKEYS {UP, DOWN, LEFT, RIGHT};
+enum MOUSE {LClick, RClick};
 int main()
 {
 	//Initialisations and Installations
@@ -12,15 +13,17 @@ int main()
 	int height = 480;
 	bool done = false;
 	bool draw = true;
-	int pos_x = width/2;
-	int pos_y = height/2;
+	int pos_x = width / 2;
+	int pos_y = height / 2;
 	int FPS = 60;
-	bool KBRDKEYS[4] = {false, false, false, false};
+	bool KBRDKEYS[4] = { false, false, false, false };
+	bool MOUSEKEYS[2] = { false, false };
 	
 	//Allegro Variable Declarations
 	ALLEGRO_DISPLAY *mainDisplay = NULL;
 	ALLEGRO_EVENT_QUEUE *mainQueue = NULL;
 	ALLEGRO_TIMER *gameLoopTimer = NULL;
+	ALLEGRO_MOUSE_STATE mouseState;
 
 	//Allegro Initialisations
 	if (!al_init()) 
@@ -29,15 +32,17 @@ int main()
 	if (!mainDisplay) 
 		return -1;
 	al_install_keyboard();
+	al_install_mouse();
 	al_init_primitives_addon();
 
 	mainQueue = al_create_event_queue();
 	gameLoopTimer = al_create_timer(1.0 / FPS);
 
 	al_register_event_source(mainQueue, al_get_keyboard_event_source());
+	al_register_event_source(mainQueue, al_get_mouse_event_source());
 	al_register_event_source(mainQueue, al_get_display_event_source(mainDisplay));
 	al_register_event_source(mainQueue, al_get_timer_event_source(gameLoopTimer));
-	
+	al_get_mouse_state(&mouseState);
 	//Timer Setup
 	al_start_timer(gameLoopTimer);
 
@@ -93,6 +98,16 @@ int main()
 				break;
 			}
 		}
+		else if (gameEvent.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+		{	
+			al_get_mouse_state(&mouseState);
+			MOUSEKEYS[LClick] = true;
+		}
+		else if (gameEvent.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+		{
+			al_get_mouse_state(&mouseState);
+			MOUSEKEYS[LClick] = false;
+		}
 		else if (gameEvent.type == ALLEGRO_EVENT_DISPLAY_CLOSE) done = true;
 		else if (gameEvent.type == ALLEGRO_EVENT_TIMER)
 		{
@@ -100,6 +115,8 @@ int main()
 			pos_x -= KBRDKEYS[LEFT] * 10;
 			pos_y += KBRDKEYS[DOWN] * 10;
 			pos_y -= KBRDKEYS[UP] * 10;
+			if (MOUSEKEYS[LClick])
+				al_draw_filled_rectangle(mouseState.x, mouseState.y, mouseState.x + 30, mouseState.y + 30, al_map_rgb(255, 0, 255));
 			draw = true;
 		}
 		if (draw && al_is_event_queue_empty(mainQueue))
